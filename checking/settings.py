020c671ddaf99e805ddcba52e63da03f26514a83
anusree promod <anusreepi@gmail.com>
2021-07-01 18:39:40 +0530

    v2

--- checking/settings.py
+++ checking/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR/'sample/TEMPLATES'
+            BASE_DIR/'firstapp/TEMPLATES'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
